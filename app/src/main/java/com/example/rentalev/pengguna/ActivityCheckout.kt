0add5ed6fea4c44c623247ce71b5b3cdc1f49a99
HelBad <helbad3@gmail.com>
2022-12-21 09:18:36 +0700

    menu checkout

--- app/src/main/java/com/example/rentalev/pengguna/ActivityCheckout.kt
+++ app/src/main/java/com/example/rentalev/pengguna/ActivityCheckout.kt
@@ -9,24 +9,29 @@ import android.text.Editable
 import android.text.InputFilter
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.rentalev.R
 import com.example.rentalev.model.Produk
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_checkout.*
-import kotlinx.android.synthetic.main.activity_detail.*
-import kotlinx.android.synthetic.main.activity_profil.*
+import java.text.DecimalFormat
+import java.text.NumberFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
 
 class ActivityCheckout : AppCompatActivity() {
     lateinit var SP: SharedPreferences
+    var formatNumber: NumberFormat = DecimalFormat("#,###")
     var formatTanggal = SimpleDateFormat("dd MMM YYYY")
-    val kalender = Calendar.getInstance()
     var formatWaktu = SimpleDateFormat("hh:mm aa")
+    val kalender = Calendar.getInstance()
+    var biaya: ArrayList<String> = arrayListOf()
+    var stokCo = ""
     var countJumlah = 0
     var countDurasi = 0
 
@@ -35,9 +40,12 @@ class ActivityCheckout : AppCompatActivity() {
         setContentView(R.layout.activity_checkout)
 
         SP = applicationContext.getSharedPreferences("Pengguna", Context.MODE_PRIVATE)
+
+        biaya = listOf("0","25000","0")
+
+        setData()
         setJumlah()
         setDurasi()
-        setData()
         tglCo.setOnClickListener {
             setTanggal()
         }
@@ -61,9 +69,51 @@ class ActivityCheckout : AppCompatActivity() {
         }
     }
 
+    private fun setData() {
+        val query = FirebaseDatabase.getInstance().getReference("produk")
+            .orderByChild("id_produk").equalTo(intent.getStringExtra("id_produk").toString())
+        query.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(datasnapshot: DataSnapshot) {
+                for (snapshot1 in datasnapshot.children) {
+                    val allocation = snapshot1.getValue(Produk::class.java)
+                    namaprodukCo.text = allocation!!.nama_produk
+                    stokCo = allocation.stok
+
+                    biaya = listOf("0","25000","0")
+                    biaya[0] = allocation.harga
+
+                    biaya.map {
+                        if(it == biaya[0]) {
+                            topics = listOf(
+                                "0","9","8","7","6","5","4","3","2","1",
+                                "Q","W","E","R","T","Y","U","I","O","P",
+                                "A","S","D","F","G","H","J","K","L",";",
+                                "Z","X","C","V","B","N","M",",",".","/"
+                            )
+                        }
+                    }
+                    hrgaCo = allocation.harga
+                    hargaCo.text = "Rp. " + formatNumber.format(hrgaCo.toInt()) + ",00"
+                    idCo.text = Editable.Factory.getInstance().newEditable(
+                        SP.getString("nama", "") + " ( "
+                                + SP.getString("nik", "") + " )"
+                    )
+                    Picasso.get().load(allocation.gambar).into(imgCo)
+
+                    val total = hrgaCo.toInt() + admin + ongkir
+                    subtotalCo.text = ": Rp. " + formatNumber.format(hrgaCo.toInt()) + ",00"
+                    adminCo.text = ": Rp. " + formatNumber.format(admin) + ",00"
+                    ongkirCo.text = ": Rp. " + formatNumber.format(ongkir) + ",00"
+                    totalCo.text = total.toString()
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
+    }
+
     private fun setJumlah() {
         plusJumlahCo.setOnClickListener {
-            if ("tersisa : " + jumlahCo.text.toString() == stokCo.text.toString()) {
+            if (jumlahCo.text.toString() == stokCo) {
             } else {
                 countJumlah = jumlahCo.text.toString().toInt()
                 countJumlah++
@@ -115,35 +165,12 @@ class ActivityCheckout : AppCompatActivity() {
     }
 
     private fun setWaktu() {
-        val waktuBooking = TimePickerDialog(this, TimePickerDialog.OnTimeSetListener {
+        val waktuBooking = TimePickerDialog(this, {
                 view, hourOfDay, minute -> val selectedTime = Calendar.getInstance()
             selectedTime.set(Calendar.HOUR_OF_DAY, hourOfDay)
             selectedTime.set(Calendar.MINUTE, minute)
             waktuCo.text = Editable.Factory.getInstance().newEditable(formatWaktu.format(selectedTime.time))
         }, kalender.get(Calendar.HOUR_OF_DAY), kalender.get(Calendar.MINUTE), false)
         waktuBooking.show()
-    }
-
-    private fun setData() {
-        val query = FirebaseDatabase.getInstance().getReference("produk")
-            .orderByChild("id_produk").equalTo(intent.getStringExtra("id_produk").toString())
-        query.addListenerForSingleValueEvent(object: ValueEventListener {
-            override fun onDataChange(datasnapshot: DataSnapshot) {
-                for (snapshot1 in datasnapshot.children) {
-                    val allocation = snapshot1.getValue(Produk::class.java)
-                    namaprodukCo.text = allocation!!.nama_produk
-                    stokCo.text = "tersisa : " + allocation.stok
-                    idCo.text = Editable.Factory.getInstance().newEditable(
-                        SP.getString("nama", "") + " ( "
-                                + SP.getString("nik", "") + " )"
-                    )
-//                    stokCo.text = allocation.stok
-//                    hargaDetail.text = "Rp. " + formatNumber.format(allocation.harga.toInt()) + ",00"
-//                    stokDetail.text = allocation.stok + " tersisa"
-//                    Picasso.get().load(allocation.gambar).into(imgDetail)
-                }
-            }
-            override fun onCancelled(databaseError: DatabaseError) {}
-        })
     }
 }
